d502eec6b8b1149b670024349787618161cc5fbd
Dima <koltundmitry99@gmail.com>
2018-11-14 20:05:59 +0300

    nothing add

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import json
 import networkx as nx
 import matplotlib.pyplot as plt
-import socket
 
 
 def parse(json_file):
-    graph = nx.Graph();
+    graph = nx.Graph()
     graph.add_nodes_from([i['idx'] for i in json_file['points']])
     graph.add_weighted_edges_from([(i['points'][0], i['points'][1], i['length']) for i in json_file['lines']])
     return graph, json_file["idx"]
@@ -27,13 +26,6 @@ def main():
     createFigures(parse(json.load(open('Graphs_JSON/custom_graph2.json'))))
     createFigures(parse(json.load(open('Graphs_JSON/custom_graph3.json'))))
     plt.show()
-    # SERVER_ADDR = 'wgforge-srv.wargaming.net'
-    # SERVER_PORT = 443
-    # sock = socket.socket()
-    # sock.connect((SERVER_ADDR,SERVER_PORT))
-    # sock.send(1)
-    # print(sock.recv(4))
-    # sock.close
 
 
 if __name__ == '__main__':
