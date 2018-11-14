d068eac356913798b6c864d80c0c3a13b47bee33
AlekseevVladimir <alekseevvladimir98@gmail.com>
2018-11-14 20:56:21 +0300

    updated main

--- src/main.py
+++ src/main.py
@@ -10,22 +10,27 @@ def parse(json_file):
     return graph, json_file["idx"]
 
 
-def createFigures(graph):
-    plt.figure(graph[1])
-    pos = nx.spring_layout(graph[0])
-    labels = nx.get_edge_attributes(graph[0], 'weight')
+def createFigures(graph, graph_idx):
+    plt.figure(graph_idx)
+    pos = nx.spring_layout(graph)
+    labels = nx.get_edge_attributes(graph, 'weight')
     nodeSize = 200
-    nx.draw(graph[0], pos, with_labels=True, nodecolor='r', edge_color='b', node_size=nodeSize, font_size=8)
-    nx.draw_networkx_edge_labels(graph[0], pos, edge_labels=labels)
+    nx.draw(graph, pos, with_labels=True, nodecolor='r', edge_color='b', node_size=nodeSize, font_size=8)
+    nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
 
 
 def main():
-    createFigures(parse(json.load(open('Graphs_JSON/small_graph.json'))))
-    createFigures(parse(json.load(open('Graphs_JSON/big_graph.json'))))
-    createFigures(parse(json.load(open('Graphs_JSON/custom_graph.json'))))
-    createFigures(parse(json.load(open('Graphs_JSON/custom_graph2.json'))))
-    createFigures(parse(json.load(open('Graphs_JSON/custom_graph3.json'))))
-    plt.show()
+	graph, graph_idx=parse(json.load(open('Graphs_JSON/small_graph.json')))
+	createFigures(graph, graph_idx)
+	graph, graph_idx=parse(json.load(open('Graphs_JSON/big_graph.json')))
+	createFigures(graph, graph_idx)
+	graph, graph_idx=parse(json.load(open('Graphs_JSON/custom_graph.json')))
+	createFigures(graph, graph_idx)
+	graph, graph_idx=parse(json.load(open('Graphs_JSON/custom_graph2.json')))
+	createFigures(graph, graph_idx)
+	graph, graph_idx=parse(json.load(open('Graphs_JSON/custom_graph3.json')))
+	createFigures(graph, graph_idx)
+	plt.show()
 
 
 if __name__ == '__main__':
